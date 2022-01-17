3623a6a4899c96ce95d2bda145580cf592258b54
Jane Nogueira <jane.tecnicavocal@gmail.com>
2022-01-17 19:05:22 -0300

    DesafioDePares-Dio

---
+++ QuadradoDePares/DesafioDePares/DesafioDePares.cs
@@ -0,0 +1,13 @@
+﻿using System; 
+
+class DesafioDePares 
+{
+    static void Main(string[] args) 
+    { 
+      int N = int.Parse(Console.ReadLine());
+            for(int i = 2; i <= N; i +=2)
+            {
+                Console.WriteLine($"{i}^2 = {i * i}");
+            }
+    }
+}
