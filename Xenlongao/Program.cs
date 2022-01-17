6004b38e0ae130abb0c65237ffe136b2bb77aad2
Jane Nogueira <jane.tecnicavocal@gmail.com>
2022-01-17 18:10:39 -0300

    Desafio Xenlongao Resolvido

---
+++ Xenlongao/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DesafioCodigoDio
+{
+    class Xenlongao
+    {
+        static void Main(string[] args)
+        {
+            int Numero = Convert.ToInt32(Console.ReadLine());
+            while(Numero -- > 0)
+            {
+                int valor = Convert.ToInt32(Console.ReadLine());
+                int novoValor = (int)Math.Sqrt(valor);     
+                int resposta = valor - novoValor;
+            }
+        }
+    }
+}
