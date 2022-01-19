3d19f92c89bf285c3905fc2c679e9e22e1da130b
Jane Nogueira <85239045+JaneNogueira@users.noreply.github.com>
2022-01-19 15:55:16 -0300

    Desafio Sucessor Par

---
+++ SucessorPar/SucessorPar/SucessorPar.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+
+class SucessorPar 
+{
+    static void Main(string[] args) { 
+
+        int x = int.Parse(Console.ReadLine());
+        if (x % 2 == 0)
+        {
+            x = x +2;
+            Console.WriteLine(x);
+        }
+        else
+        {
+            x = x + 1;
+            Console.WriteLine(x);
+        }
+    }
+}
