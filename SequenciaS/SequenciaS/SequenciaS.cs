f9074e3c5519aeb129e98bd5ab4a0f75ff929eb7
Jane Nogueira <85239045+JaneNogueira@users.noreply.github.com>
2022-01-19 14:56:21 -0300

    Add files via upload

---
+++ SequenciaS/SequenciaS/SequenciaS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class SequenciaS
+{
+    static void Main(string[] args)
+    {
+        double c, S = 0;
+        for (double n = 1; n <= 100; n++)
+        {
+            c = 1 / n;
+            S += c;
+        }
+        var x = Math.Round(S, 2);
+        Console.WriteLine(x);
+    }
+}
