127ebb105b731a50bfa76352505b409e8f500eb5
Jane Nogueira <85239045+JaneNogueira@users.noreply.github.com>
2022-01-28 23:01:44 -0300

    Desafios de Código Bootcamp DIO

---
+++ DesafiosCsharp/DesafioCsharp/Divisao/DivisaoXY.cs
@@ -0,0 +1,23 @@
+namespace DesafioCsharp.Divisao
+{
+    public class DivisaoXY
+    {
+        static void Main() {
+        int limit = Int32.Parse(Console.ReadLine());
+        for (int i = 0; i < limit; i++) 
+        {
+            string[] line = Console.ReadLine().Split(" ");
+            double X = double.Parse(line[0]);
+            double Y = double.Parse(line[1]);
+            if (   ) 
+            {
+                
+            } 
+
+            else 
+            {
+                
+            }
+        }
+    }
+}
